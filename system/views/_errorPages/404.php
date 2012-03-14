f7cc1c701762266e994fdd378ad881168ffb3273
Klederson Bueno <klederson@klederson.com>
2012-03-14 02:39:34 -0300

    Analytics

--- system/views/_errorPages/404.php
+++ system/views/_errorPages/404.php
@@ -300,7 +300,19 @@
       </div><!-- End of bottom footer -->
 
     </div><!-- End of bottom footer wrapper -->
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1167851-14']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
   </body>
 
 </html>
