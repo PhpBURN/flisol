663cdd24bb5cdf284a5b72ae593bedc8c1afd140
Klederson Bueno <klederson@klederson.com>
2012-03-14 01:07:11 -0300

    PhpBURN Footer

--- system/views/principal/callforpapers.php
+++ system/views/principal/callforpapers.php
@@ -1,1 +1,68 @@
-<iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dHJjS3lOVmFidWlEQlU2S09TWWMwLVE6MQ" width="100%" height="2300" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+<!-- Start of left content wrapper -->
+<div class="left_content_wrapper">
+
+  <!-- Start of post -->
+  <div class="post">
+
+    <h4>Dados para Inscrição</h4>
+
+    <p>Cadastre sua palestra e aguarde ser convocado! Seja Open Source, Seja Livre!</p>
+
+    <iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dHJjS3lOVmFidWlEQlU2S09TWWMwLVE6MQ" width="100%" height="2300" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>      
+
+  </div><!-- End of post -->
+
+</div><!-- End of left content wrapper -->
+
+<!-- Start of right content wrapper -->
+<div class="right_content_wrapper">
+
+  <!-- Start of text widget -->
+  <div class="text_widget">
+
+    <h2>FLISOL 2012</h2>
+
+    <ul class="contact">
+      <li><span class="contact_title">Quando?</span></li>
+      <li>Sábado | 28 de Abril</li>
+      <li>09:00 - 17:00</li>
+    </ul>        
+
+    <ul class="contact">
+      <li><span class="contact_title">Onde?</span></li>
+      <li><a href="http://www.policamp.edu.br" target="_blank">Falculdade POLICAMP</a></li>
+      <li>Rua Luiz Otávio, 1281 | Pq. Sta. Cândida</li>
+      <li>Campinas | São Paulo</li>
+    </ul>
+
+    <ul class="contact">
+      <li><span class="contact_title">Coordenação ( Campinas )</span></li>
+      <li><a href="mailto:klederson@klederson.com">Klederson Bueno</a> | Geral</li>
+      <li><a href="mailto:ricardo@ricardopontes.net">Ricardo Pontes</a> | Relacionamento  </li>
+      <li><a href="#">André Déo</a> | Logístico</li>
+      <li><a href="#">Clóvis Tristão</a> | Conteúdo </li>
+    </ul>
+    
+    <ul class="contact">
+      <li><span class="contact_title">Colaboradores</span></li>
+      <li><a href="mailto:ericatamiris@gmail.com">Érica Tamiris</a> | Mat. Gráfico</li>
+      <li><a href="mailto:klederson@klederson.com">Klederson Bueno</a> | Website </li>
+      <li><a href="#">Ricardo Pontes</a> | Brindes </li>
+      <li><a href="#">André Déo</a> | Local e Infra</li>
+      <li><a href="http://lhc.net.br">Lab. Hacker Campinas</a> | Workshops</li>
+      <li><a href="http://garoa.net.br">Garoa Hacker Club</a> | Workshops</li>
+    </ul>
+    
+
+  </div><!-- End of text widget -->  
+
+  <!-- Start of text widget -->
+  <div class="text_widget">
+
+    <h2>Como Chegar?</h2>
+
+    <iframe width="auto" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=-22.85170,-47.04363&amp;output=embed"></iframe><br />
+
+  </div><!-- End of text widget -->    
+
+</div><!-- End of right content wrapper -->
